62735ae39995c4d1c671b826e8e281871ee392cf
Prajwal Shetty <shettyprajwal333@gmail.com>
2025-07-13 20:20:39 +0530

    added changes to footer

--- src/app/electricalsvctypes/electricalsvctypes-routing.module.ts
+++ src/app/electricalsvctypes/electricalsvctypes-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ElctricalsvcComponent } from './elctricalsvc/elctricalsvc.component';
 import { SolortypesComponent } from './solortypes/solortypes.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'solar',
     component: SolortypesComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
   }
 ];
 

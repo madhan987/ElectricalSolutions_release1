672c39043c0aa4bdc0531c242a62b02d3ca36781
Prajwal Shetty <shettyprajwal333@gmail.com>
2025-08-04 21:50:16 +0530

    Added contact us page

--- src/app/electricalsvctypes/electricalsvctypes-routing.module.ts
+++ src/app/electricalsvctypes/electricalsvctypes-routing.module.ts
@@ -1,32 +1,36 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ElctricalsvcComponent } from './elctricalsvc/elctricalsvc.component';
-import { SolortypesComponent } from './solortypes/solortypes.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ServicesComponent } from './services/services.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ElctricalsvcComponent } from "./elctricalsvc/elctricalsvc.component";
+import { SolortypesComponent } from "./solortypes/solortypes.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+import { ServicesComponent } from "./services/services.component";
+import { ContactUsComponent } from "./contact-us/contact-us.component";
 
 const routes: Routes = [
   {
-    path: 'elc-type',
-    component: ElctricalsvcComponent
+    path: "elc-type",
+    component: ElctricalsvcComponent,
   },
   {
-    path: 'solar',
-    component: SolortypesComponent
+    path: "solar",
+    component: SolortypesComponent,
   },
   {
-    path: 'about-us',
-    component: AboutUsComponent
+    path: "about-us",
+    component: AboutUsComponent,
   },
   {
-    path: 'services',
-    component: ServicesComponent
-  }
+    path: "services",
+    component: ServicesComponent,
+  },
+  {
+    path: "contact-us",
+    component: ContactUsComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ElectricalsvctypesRoutingModule { }
+export class ElectricalsvctypesRoutingModule {}
